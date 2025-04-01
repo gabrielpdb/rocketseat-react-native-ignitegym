495c3884b4a314140413609c53aa8f7121a8c1fb
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-03-31 22:40:43 -0300

    Imagem de Background

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import {
 import { Center, GluestackUIProvider, Text } from "@gluestack-ui/themed"
 import { config } from "./config/gluestack-ui.config"
 import { Loading } from "@components/Loading"
+import { SignIn } from "@screens/SignIn"
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -17,13 +18,7 @@ export default function App() {
         backgroundColor={"transparent"}
         translucent
       />
-      {fontsLoaded ? (
-        <Center flex={1} bg="$gray700">
-          <Text color="white">Home</Text>
-        </Center>
-      ) : (
-        <Loading />
-      )}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </GluestackUIProvider>
   )
 }

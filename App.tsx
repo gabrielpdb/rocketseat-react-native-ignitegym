71272d5f98a229b6cb9191c8b09574b824278fa0
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-04-01 22:49:54 -0300

    Criando a SignUp

--- App.tsx
+++ App.tsx
@@ -4,10 +4,11 @@ import {
   Roboto_700Bold,
   Roboto_400Regular,
 } from "@expo-google-fonts/roboto"
-import { Center, GluestackUIProvider, Text } from "@gluestack-ui/themed"
+import { GluestackUIProvider } from "@gluestack-ui/themed"
 import { config } from "./config/gluestack-ui.config"
 import { Loading } from "@components/Loading"
 import { SignIn } from "@screens/SignIn"
+import { SignUp } from "@screens/SignUp"
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -18,7 +19,7 @@ export default function App() {
         backgroundColor={"transparent"}
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </GluestackUIProvider>
   )
 }

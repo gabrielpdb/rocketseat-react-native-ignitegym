260dea7db3ed57f0f59be1dd350c9bad4bede9c4
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-04-01 23:28:59 -0300

    Aplicando navegação de autenticação

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -11,8 +11,16 @@ import BackgroundImg from "@assets/background.png"
 import Logo from "@assets/logo.svg"
 import { Input } from "@components/Input"
 import { Button } from "@components/Button"
+import { useNavigation } from "@react-navigation/native"
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes"
 
 export function SignIn() {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>()
+
+  function handleNewAccount() {
+    navigation.navigate("signUp")
+  }
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
@@ -51,7 +59,11 @@ export function SignIn() {
             <Text color="$gray100" fontSize={"$sm"} mb="$3" fontFamily="$body">
               Ainda não tem acesso?
             </Text>
-            <Button title="Criar conta" variant="outline" />
+            <Button
+              title="Criar conta"
+              variant="outline"
+              onPress={handleNewAccount}
+            />
           </Center>
         </VStack>
       </VStack>

113d5ed851a7d94591dd9d785531c89a05f1751d
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-04-03 22:49:55 -0300

    Listando os Grupos

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,24 +2,29 @@ import { Group } from "@components/Group"
 import { HomeHeader } from "@components/HomeHeader"
 import { HStack, VStack } from "@gluestack-ui/themed"
 import { useState } from "react"
+import { FlatList } from "react-native"
 
 export function Home() {
-  const [groupSelected, setGroupSelected] = useState("costa")
+  const [groups, setGroups] = useState(["Costas", "Bíceps", "Tríceps", "Ombro"])
+  const [groupSelected, setGroupSelected] = useState("Costas")
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <HStack>
-        <Group
-          name="Costas"
-          isActive={groupSelected === "costa"}
-          onPress={() => setGroupSelected("costa")}
-        />
-        <Group
-          name="Ombro"
-          isActive={groupSelected === "ombro"}
-          onPress={() => setGroupSelected("ombro")}
-        />
-      </HStack>
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+      />
     </VStack>
   )
 }

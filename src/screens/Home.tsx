d9f7c64116a3f5c57804e0035076eab081e2554a
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-04-03 21:20:32 -0300

    Posicionando na vertical e horizontal

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,10 @@
+import { HomeHeader } from "@components/HomeHeader"
 import { Center, Text } from "@gluestack-ui/themed"
 
 export function Home() {
   return (
     <Center flex={1}>
-      <Text>Home</Text>
+      <HomeHeader />
     </Center>
   )
 }

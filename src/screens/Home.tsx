9c8cc48909b16655e181431de5aa40c88cecfe05
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-04-04 21:12:56 -0300

    Navegando para tela de exercício

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,6 +2,8 @@ import { ExerciseCard } from "@components/ExerciseCard"
 import { Group } from "@components/Group"
 import { HomeHeader } from "@components/HomeHeader"
 import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed"
+import { useNavigation } from "@react-navigation/native"
+import { AppNavigatorRoutesProps } from "@routes/app.routes"
 import { useState } from "react"
 import { FlatList } from "react-native"
 
@@ -14,6 +16,13 @@ export function Home() {
   ])
   const [groups, setGroups] = useState(["Costas", "Bíceps", "Tríceps", "Ombro"])
   const [groupSelected, setGroupSelected] = useState("Costas")
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate("exercise")
+  }
+
   return (
     <VStack flex={1}>
       <HomeHeader />
@@ -45,7 +54,9 @@ export function Home() {
         <FlatList
           data={exercises}
           keyExtractor={(item) => item}
-          renderItem={({ item }) => <ExerciseCard />}
+          renderItem={({ item }) => (
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
+          )}
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{ paddingBottom: 20 }}
         />

f217ee27290fff5e134a6ff848f18608f975c8a0
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-04-07 20:28:45 -0300

    Criar o componente de toast

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -7,6 +7,7 @@ import { Alert, ScrollView, TouchableOpacity } from "react-native"
 import * as ImagePicker from "expo-image-picker"
 import * as FileSystem from "expo-file-system"
 import { useState } from "react"
+import { ToastMessage } from "@components/ToastMessage"
 
 export function Profile() {
   const [userPhoto, setUserPhoto] = useState("https:github.com/gabrielpdb.png")
@@ -47,6 +48,14 @@ export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+
+      <ToastMessage
+        id={"1"}
+        title="Exemplo"
+        description="lkfjdsalfjlsadlsadjflsadjfsaj;"
+        action="success"
+        onClose={() => {}}
+      />
 
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">

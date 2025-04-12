03d92b782567a24ab4782f137f61d84021d0a8ae
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-04-11 22:00:10 -0300

    Rules Patterns

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -90,6 +90,13 @@ export function SignUp() {
             <Controller
               control={control}
               name="email"
+              rules={{
+                required: "Informe o e-mail",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "E-mail inválido",
+                },
+              }}
               render={({ field: { onChange, value } }) => (
                 <Input
                   placeholder="E-mail"
@@ -100,6 +107,10 @@ export function SignUp() {
                 />
               )}
             />
+
+            {errors.email?.message && (
+              <Text color="$white">{errors.email.message}</Text>
+            )}
 
             <Controller
               control={control}

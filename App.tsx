ce25f99829244de0806983d11e5d4ffaccac3cef
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-04-21 20:26:20 -0300

    Unificando o create e provider context

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import { GluestackUIProvider } from "@gluestack-ui/themed"
 import { config } from "./config/gluestack-ui.config"
 import { Loading } from "@components/Loading"
 import { Routes } from "@routes/index"
-import { AuthContext } from "@contexts/AuthContext"
+import { AuthContextProvider } from "@contexts/AuthContext"
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -19,18 +19,9 @@ export default function App() {
         backgroundColor={"transparent"}
         translucent
       />
-      <AuthContext.Provider
-        value={{
-          user: {
-            id: "1",
-            name: "Gabriel",
-            email: "gabriel@email.com",
-            avatar: "gabriel.png",
-          },
-        }}
-      >
+      <AuthContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </GluestackUIProvider>
   )
 }

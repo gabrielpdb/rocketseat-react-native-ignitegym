ad8a5e364d173eaa77eb3c20073a11f7e972d43a
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-04-22 12:45:47 -0300

    Buscando dados do usuário no back-end

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,10 @@
 import { UserDTO } from "@dtos/UserDTO"
+import { api } from "@services/api"
 import { createContext, ReactNode, useContext, useState } from "react"
 
 export type AuthContextDataProps = {
   user: UserDTO
-  signIn: (email: string, password: string) => void
+  signIn: (email: string, password: string) => Promise<void>
 }
 
 type AuthContextProviderProps = {
@@ -15,20 +16,18 @@ export const AuthContext = createContext<AuthContextDataProps>(
 )
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
-  const [user, setUser] = useState({
-    id: "1",
-    name: "Gabriel",
-    email: "gabriel@email.com",
-    avatar: "gabriel.png",
-  })
+  const [user, setUser] = useState<UserDTO>({} as UserDTO)
 
-  function signIn(email: string, password: string) {
-    setUser({
-      name: "",
-      email,
-      id: "",
-      avatar: "",
-    })
+  async function signIn(email: string, password: string) {
+    try {
+      const { data } = await api.post("/sessions", { email, password })
+
+      if (data.user) {
+        setUser(data.user)
+      }
+    } catch (error) {
+      throw error
+    }
   }
 
   return (

f9abca6e31ecb4e8211f539f9788edc0b266292c
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-05-08 22:01:36 -0300

    Armazenando o Refresh Token

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -43,11 +43,15 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     setUser(userData)
   }
 
-  async function storageUserAndTokenSave(userData: UserDTO, token: string) {
+  async function storageUserAndTokenSave(
+    userData: UserDTO,
+    token: string,
+    refresh_token: string
+  ) {
     try {
       setIsLoadingUserStorageData(true)
       await storageUserSave(userData)
-      await storageAuthTokenSave(token)
+      await storageAuthTokenSave({ refresh_token, token })
     } catch (error) {
       throw error
     } finally {
@@ -59,8 +63,8 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     try {
       const { data } = await api.post("/sessions", { email, password })
 
-      if (data.user && data.token) {
-        await storageUserAndTokenSave(data.user, data.token)
+      if (data.user && data.token && data.refresh_token) {
+        await storageUserAndTokenSave(data.user, data.token, data.refresh_token)
 
         userAndTokenUpdate(data.user, data.token)
       }
@@ -98,7 +102,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       setIsLoadingUserStorageData(true)
 
       const userLogged = await storageUserGet()
-      const token = await storageAuthTokenGet()
+      const { token } = await storageAuthTokenGet()
 
       if (token && userLogged) {
         userAndTokenUpdate(userLogged, token)

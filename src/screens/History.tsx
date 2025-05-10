94d22b9c7092559021cad37505e8336402b1e355
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-05-09 21:52:14 -0300

    Loading no carregamento do histórico

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,4 +1,5 @@
 import { HistoryCard } from "@components/HistoryCard"
+import { Loading } from "@components/Loading"
 import { ScreenHeader } from "@components/ScreenHeader"
 import { HistoryGroupByDayDTO } from "@dtos/HistoryGroupByDayDTO"
 import {
@@ -55,26 +56,30 @@ export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de exercícios" />
-      <SectionList
-        sections={exercises}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <HistoryCard data={item} />}
-        renderSectionHeader={({ section }) => (
-          <Heading color="$gray200" fontSize={"$md"} mt="$10" mb={"$3"}>
-            {section.title}
-          </Heading>
-        )}
-        style={{ paddingHorizontal: 32 }}
-        contentContainerStyle={
-          exercises.length === 0 && { flex: 1, justifyContent: "center" }
-        }
-        ListEmptyComponent={() => (
-          <Text textAlign="center" color="$gray100">
-            Não há exercícios registrados ainda.{"\n"} Vamos treinar?
-          </Text>
-        )}
-        showsVerticalScrollIndicator={false}
-      />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <SectionList
+          sections={exercises}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <HistoryCard data={item} />}
+          renderSectionHeader={({ section }) => (
+            <Heading color="$gray200" fontSize={"$md"} mt="$10" mb={"$3"}>
+              {section.title}
+            </Heading>
+          )}
+          style={{ paddingHorizontal: 32 }}
+          contentContainerStyle={
+            exercises.length === 0 && { flex: 1, justifyContent: "center" }
+          }
+          ListEmptyComponent={() => (
+            <Text textAlign="center" color="$gray100">
+              Não há exercícios registrados ainda.{"\n"} Vamos treinar?
+            </Text>
+          )}
+          showsVerticalScrollIndicator={false}
+        />
+      )}
     </VStack>
   )
 }
